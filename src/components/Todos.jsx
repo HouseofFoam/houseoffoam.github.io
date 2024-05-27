7692dd398b993f8f7c7198f7e56e40f173fcfb81
Ifan P <90776063+HouseofFoam@users.noreply.github.com>
2024-05-27 12:10:56 +0700

    Adding inline css

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -4,12 +4,19 @@ import TodoItem from "./TodoItem";
 
 function Todo({ todos }) {
   return (
-    <div>
+    <div style={styles.container}>
       {todos.map((todo) => {
         return <TodoItem key={todo.id} todo={todo} />;
       })}
     </div>
   );
 }
+
+const styles = {
+  container: {
+    width: "40%",
+    margin: "0 auto",
+  },
+};
 
 export default Todo;
